00ed8cf90955462c275fe3344d597e78025a2473
Sammi Husky <sammi-husky@live.com>
2022-04-10 15:34:46 -0700

    Initial push

---
+++ source/rel.cpp
@@ -0,0 +1,42 @@
+#include "brawl/gf/gfTask.h"
+
+namespace stFinal {
+    
+extern "C" {
+    typedef void (*PFN_voidfunc)();
+    __declspec(section ".init") extern PFN_voidfunc _ctors[];
+    __declspec(section ".init") extern PFN_voidfunc _dtors[];
+
+    void _prolog();
+    void _epilog();
+    void _unresolved();
+}
+
+extern void main();
+
+void _prolog() {
+    // Run global constructors
+    PFN_voidfunc *ctor;
+    for (ctor = _ctors; *ctor; ctor++)
+    {
+        (*ctor)();
+    }
+    
+
+    stFinal::main();
+}
+
+void _epilog() {
+    // run the global destructors
+    PFN_voidfunc *dtor;
+    for (dtor = _dtors; *dtor; dtor++)
+    {
+        (*dtor)();
+    }
+}
+
+void _unresolved(void) {
+    
+}
+
+}
---
+++ source/test.cpp
@@ -0,0 +1,22 @@
+#include "brawl/gf/gfTask.h"
+
+namespace stFinal {
+
+gfTask globalTask = gfTask("test",0,1,2,3);
+static const float SOME_VAL = 1.0f;
+
+bool doAThing(float f1, float f2){
+    return f1 > f2;
+}
+ 
+int main() {
+    float f = SOME_VAL / 2;
+    bool a = doAThing(SOME_VAL, f);
+
+    if(a){
+        globalTask.processDefault();
+    }
+    return 0;
+}
+
+}
