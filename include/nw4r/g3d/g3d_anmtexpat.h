cfe3ff122e08c9b1440afeffec79d539f62b92da
Kapedani <kapedanipm@gmail.com>
2022-04-21 02:38:38 -0400

    Defined classes that get used in Ground

---
+++ include/nw4r/g3d/g3d_anmtexpat.h
@@ -0,0 +1,32 @@
+#pragma once
+
+class AnmObjTexPatRes {
+public:
+    virtual u32 IsDerivedFrom(int* unk1);
+    virtual void G3dProc(int unk1, int unk2, int unk3);
+    virtual ~AnmObjTexPatRes();
+    virtual int* GetTypeObj();
+    virtual int GetTypeName();
+    virtual void SetFrame(double unk1);
+    virtual double GetFrame();
+    virtual void UpdateFrame();
+    virtual void SetUpdateFrame(double unk1);
+    virtual double GetUpdateFrame();
+    virtual int Bind(int* unk1);
+    virtual void Release();
+    virtual int* GetResult(int* unk1, int unk2);
+    virtual int Attach(int unk1, int* unk2); // TODO
+    virtual int* Detach(int unk1); // TODO
+    virtual void DetachAll();
+
+    AnmObjTexPatRes* Construct(int* unk1, int* unk2, int* unk3, int* unk4, u32 unk5);
+
+
+    char _spacer[40];
+
+    int* anmTexPatFile;
+
+    char _spacer2[4];
+};
+
+// Size: 52
