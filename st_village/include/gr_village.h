b613d90fa3a2e37a228d39de25bc7971d6ca9991
Sammi Husky <sammi-husky@live.com>
2022-09-23 13:49:15 -0700

    Lot of work for st_village + move .lst to submodules

---
+++ st_village/include/gr_village.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <gr/gr_yakumono.h>
+const float UNK_FLOAT = 0.0f;
+class grVillage : public grYakumono
+{
+    protected:
+        u32 unk1;
+        float unk_float;
+        u32 sceneWork;
+        u32 sceneBit;
+        u32 stateWork;
+        u32 posGuestWork;
+    public:
+        grVillage(char* taskName) : grYakumono(taskName) {
+            unk1 = 0;
+            unk_float = UNK_FLOAT;
+            sceneWork = 0;
+            sceneBit = 0;
+            stateWork = 0;
+            posGuestWork = 0;
+        };
+        virtual void update(float unk1);
+        virtual void updateVisible(float unk1);
+        virtual void setSceneWork(u32 sceneWork);
+        virtual void setSceneBit(u32 sceneBit);
+        virtual void setStateWork(u32 stateWork);
+        virtual void setPosGuestWork(u32 posGuestWork);
+        virtual bool isSceneBit();
+        virtual ~grVillage() { };
+
+        static grVillage* create(int mdlIndex, char* tgtNodeName, char* taskName);
+        int adventureEventGetItem();
+        int getInitializeInfo();
+        void setInitializeInfo();
+        void setInitializeFlag();
+        void disableCalcCollision();
+        void enableCalcCollision();
+        bool isEnableCalcCollision();
+        short getMdlIndex();
+        void initStageData();
+        float* getStageData();
+        char getTransparencyFlag();
+        u32 getGimmickData();
+        void setGimmickData(u32 data);
+        int getModelCount();
+
+};
---
+++ st_village/include/gr_village_balloon.h
@@ -0,0 +1,7 @@
+#pragma once
+
+class grVillageBalloon : grVillage
+{
+public:
+    void create(int modelIndex, char* tgtNodeName, char* taskName);
+};
