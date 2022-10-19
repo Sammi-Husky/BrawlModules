d11c77a7b1e76752c628db6613f7abfafbcd673f
Kapedani <kapedanipm@gmail.com>
2022-10-19 09:08:55 -0400

    Start of reimpl grAdventureDoor to learn more how AreaGimmicks work

---
+++ st_demo/include/gr_adventure_door.h
@@ -0,0 +1,78 @@
+#pragma once
+
+#include <gr/gr_yakumono.h>
+#include <st/st_trigger.h>
+
+struct DoorData {
+    grGimmickMotionPathData motionPathData;
+    char _spacer[21];
+    float field_0x20;
+    float field_0x24;
+    float field_0x28;
+    float field_0x2c;
+    char levelId;
+    char levelSequenceId;
+    char levelSegmentId;
+    char doorIndex;
+    char field_0x34;
+    char _spacer2[1];
+    char mdlIndex;
+    char _spacer3[1];
+    float xPos;
+    float yPos;
+    TriggerData openDoorTriggerData;
+    char _spacer4[8];
+    TriggerData motionPathTriggerData;
+    TriggerData isValidTriggerData;
+    float difficultyMotionRatios[15];
+};
+
+class grAdventureDoor : public grYakumono
+{
+protected:
+    int field_0x110;
+    float field_0x114;
+    float field_0x118;
+    float field_0x11c;
+    DoorData* doorData;
+    unsigned int jumpData;
+    int utArchiveType;
+    grYakumonoAreaData areaData;
+    grYakumonoAreaInit areaInit;
+    grYakumonoAreaInfo areaInfo;
+public:
+    grAdventureDoor(char* taskName) : grYakumono(taskName) {
+        field_0x110 = 0.0;
+        field_0x114 = 0.0;
+        field_0x118 = 0.0;
+        field_0x11c = 50.0;
+        doorData = NULL;
+        jumpData = 0;
+        utArchiveType = 0x28;
+        areaInfo.field_0x0 = 0;
+        areaInfo.field_0x4 = 0;
+    };
+    virtual void update(float frameDiff);
+    virtual void startup(gfArchive* data, u32 unk1, u32 unk2);
+    virtual void onGimmickEvent(int* unk1, int* taskId);
+    virtual ~grAdventureDoor() { };
+    virtual void setJumpData(u32 jumpData);
+    virtual void setInitializeFlag();
+    virtual void openTheDoor();
+    virtual void EachDoorTypeEffect();
+
+    static grAdventureDoor* create(int mdlIndex, u32 jumpData, char* taskName);
+    int adventureEventGetItem();
+    int getInitializeInfo();
+    void setInitializeInfo();
+    void disableCalcCollision();
+    void enableCalcCollision();
+    bool isEnableCalcCollision();
+    short getMdlIndex();
+    void initStageData();
+    float* getStageData();
+    char getTransparencyFlag();
+    void* getGimmickData();
+    void setGimmickData(void* gimmickData);
+    int getModelCount();
+};
---
+++ st_demo/source/st_demo.cpp
@@ -0,0 +1,183 @@
+#include <memory.h>
+#include <st/st_class_info.h>
+#include "st_demo.h"
+#include <OSError.h>
+
+static stClassInfoImpl<2, stDemo> classInfo = stClassInfoImpl<2, stDemo>();
+
+stDemo* stDemo::create() {
+    stDemo* stage = new(StageInstance) stDemo(0x2);
+    return stage;
+}
+bool stDemo::loading(){
+   return true;
+}
+void stDemo::update(float frameDiff){
+   return;
+}
+
+void stDemo::createObj() {
+   testStageParamInit(fileData, 0xA);
+   testStageDataInit(fileData, 0x14, 1);
+   grFinal* ground = grFinal::create(1, "", "grFinalMainBg");
+   if(ground != NULL){
+      addGround(ground);
+      ground->startup(fileData,0,0);
+      ground->setStageData(stageData);
+      ground->setType(0);
+      ground->setDontMoveGround();
+   }
+   ground = grFinal::create(2, "", "grFinalStage");
+   if(ground != NULL){
+      addGround(ground);
+      ground->startup(fileData,0,0);
+      ground->setStageData(stageData);
+      ground->setType(1);
+      ground->setDontMoveGround();
+   }
+    grAdventureDoor* door = grAdventureDoor::create(102, 0x28000204, "grAdventureDoor");
+    if(door != NULL){
+        addGround(door);
+        door->setGimmickData(&this->doorData);
+        door->startup(fileData,0,0);
+    }
+
+   createCollision(fileData, 2, NULL);
+   initCameraParam();
+   void* posData = fileData->getData(DATA_TYPE_MODEL, 0x64, 0xfffe);
+   if(posData == NULL){
+      // if no stgPos model in pac, use defaults
+      createStagePositions();
+   } 
+   else {
+      //stgPosWrapper stgPos = {posData}; // creates wrapper on the stack
+      createStagePositions(&posData);
+   }
+   createWind2ndOnly();
+   loadStageAttrParam(fileData, 0x1E);
+   void* scnData = fileData->getData(DATA_TYPE_SCENE, 0, 0xfffe);
+   registSceneAnim(scnData, 0);
+   initPosPokeTrainer(1, 0);
+   createObjPokeTrainer(fileData, 0x65, "PokeTrainer00", this->unk, 0x0);
+}
+
+void Ground::setStageData(float* stageData) {
+   this->stageData = stageData;
+}
+void stDemo::startFighterEvent() {
+   return;
+}
+int stDemo::initializeFighterAttackRatio() {
+   return 0;
+}
+int stDemo::helperStarWarp(){
+   return 0;
+}
+bool stDemo::isSimpleBossBattleMode() {
+   return false;
+}
+bool stDemo::isBossBattleMode() {
+   return false;
+}
+bool stDemo::isCameraLocked() {
+   return true;
+}
+void stDemo::notifyTimmingGameStart(){
+   return;
+}
+float stDemo::getFrameRuleTime() {
+   return this->frameRuleTime;
+}
+void stDemo::setFrameRuleTime(float newTime) {
+   this->frameRuleTime = newTime;
+}
+bool stDemo::isNextStepBgmEqualNowStepBgmFromFlag() {
+   return false;
+}
+float stDemo::getBgmPlayOffsetFrame(){
+   return BGM_PLAY_OFFSET_FRAME;
+}
+float stDemo::getBgmVolume() {
+   return BGM_VOLUME;
+}
+void stDemo::setBgmChange(float unk, u32 unk1, u32 unk2) {
+   this->unk2 = unk1;
+   this->unk3 = unk2;
+   this->unk4 = unk;
+}
+void stDemo::getBgmChangeID(u32 unk1, float unk2) {
+   unk1 = this->unk3;
+   unk2 = this->unk4;
+}
+bool stDemo::isBgmChange() {
+   return this->unk2;
+}
+int stDemo::getBgmOptionID() {
+   return 0;
+}
+int stDemo::getNowStepBgmID() {
+   return 0;
+}
+int stDemo::getBgmID() {
+   return 0;
+}
+int stDemo::getBgmIDOverload() {
+   return 0;
+}
+void stDemo::appearanceFighterLocal() {
+   return;
+}
+// TODO
+// stubbed because i can't figure out where this is called
+int stDemo::getScrollDir(u32 unk1) {
+   return 0;
+}
+int stDemo::getDefaultLightSetIndex(){
+   return 0x14;
+}
+int stDemo::getAIRange() {
+   return (u32) &this->aiRange;
+}
+bool stDemo::isAdventureStage(){
+   return false;
+}
+int stDemo::getPokeTrainerDrawLayer() {
+   return 0;
+}
+float stDemo::getPokeTrainerPositionZ() {
+   return POKETRAINER_Z;
+}
+void stDemo::getPokeTrainerPointData(int * unk, int unk2){
+   return;
+}
+int stDemo::getPokeTrainerPointNum() {
+   return 0;
+}
+bool stDemo::isReStartSamePoint() {
+   return true;
+}
+int stDemo::getWind2ndOnlyData() {
+   return (u32) &this->wndOnlyData2;
+}
+bool stDemo::isBamperVector() {
+   return true;
+}
+int stDemo::getFinalTechniqColor() {
+   return 0x14000496;
+}
+
+template<int I, typename T>
+T* stClassInfoImpl<I, T>::create(){
+    T* stage = new(StageInstance) T(I);
+    return stage;
+}
+
+template<int I, typename T>
+stClassInfoImpl<I, T>::~stClassInfoImpl() {
+   setClassInfo(I,0);
+}
+
+template<int I, typename T>
+void stClassInfoImpl<I, T>::preload() {
+   return;
+}
