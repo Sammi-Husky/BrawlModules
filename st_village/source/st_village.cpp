a2bca6df2fe7e4230a327dd7dd428270c72457d0
Sammi Husky <sammi-husky@live.com>
2022-10-23 14:43:36 -0700

    Quite a bit moore  work on st_village

--- st_village/source/st_village.cpp
+++ st_village/source/st_village.cpp
@@ -6,25 +6,30 @@
 #include <st/st_melee.h>
 #include "st_village.h"
 #include "gr_village.h"
+#include "gr_village_sky.h"
 
 static stClassInfoImpl<0x21, stVillage> classInfo = stClassInfoImpl<0x21, stVillage>();
 
-stVillage* stVillage::create() {
-    stVillage* stage = new(StageInstance) stVillage(0x21);
-    return stage;
+stVillage *stVillage::create()
+{
+   stVillage *stage = new (StageInstance) stVillage(0x21);
+   return stage;
 }
-bool stVillage::loading(){
+bool stVillage::loading()
+{
    return true;
 }
-void stVillage::update(float frameDiff){
-   this->updatePerio(unk1);
-   this->updateTaxi(unk1);
-   this->updateUFO(unk1);
+void stVillage::update(float frameDelta)
+{
+   this->updatePerio(frameDelta);
+   this->updateTaxi(frameDelta);
+   this->updateUFO(frameDelta);
    return;
 }
 
-extern sndSystem* g_sndSystem;
-void stVillage::createObj() {
+extern sndSystem *g_sndSystem;
+void stVillage::createObj()
+{
    testStageParamInit(fileData, 0xA);
    testStageDataInit(fileData, 0x14, 0x48);
    this->initStageDataTbl();
@@ -34,178 +39,270 @@ void stVillage::createObj() {
    this->createObjBg(0x2);
    this->createObjBg(0x3);
    this->createObjBg(0x4);
-   this->createObjBg(0x5);
-   this->createObjBg(0x6);
-   this->createObjBg(0x7);
-   this->createObjGuestPathMove(0x8);
-   this->createObjGuestPathMove(0x9);
-   this->createObjGuestPathMove(0xA);
-   this->createObjGuestPathMove(0xB);
-   this->createObjLiveDeco(0xC);
-   this->createObjClock(0xD);
-   this->createObjBalloon(0xE);
-   this->createObjGuest();
+   this->createObjSky(0x5);
+   this->createObjAshiba(0x6);        // TODO
+   this->createObjAshiba(0x7);        // TODO
+   this->createObjGuestPathMove(0x8); // TODO
+   this->createObjGuestPathMove(0x9); // TODO
+   this->createObjGuestPathMove(0xA); // TODO
+   this->createObjGuestPathMove(0xB); // TODO
+   this->createObjLiveDeco(0xC);      // TODO
+   this->createObjClock(0xD);         // TODO
+   this->createObjBalloon(0xE);       // TODO
+   this->createObjGuest();            // TODO
 
    createCollision(this->fileData, 2, 0);
    this->initCameraParam();
 
-   void* posData = fileData->getData(DATA_TYPE_MODEL, 0x64, 0xfffe);
-   if(posData == NULL){
+   void *posData = fileData->getData(DATA_TYPE_MODEL, 0x64, 0xfffe);
+   if (posData == NULL)
+   {
       // if no stgPos model in pac, use defaults
       createStagePositions();
-   } 
-   else {
+   }
+   else
+   {
       createStagePositions(&posData);
    }
 
    createWind2ndOnly();
    loadStageAttrParam(fileData, 0x1E);
-   
-   void* scnData = fileData->getData(DATA_TYPE_SCENE, 0, 0xfffe);
-   registSceneAnim(scnData, this->unk1);
-   
+
+   void *scnData = fileData->getData(DATA_TYPE_SCENE, 0, 0xfffe);
+   registSceneAnim(scnData, this->scene);
+
    initPosPokeTrainer(1, 0);
    createObjPokeTrainer(fileData, 0x65, "PokeTrainer00", this->unk, 0x0);
-   
-   switch (this->unk1)
+
+   switch (this->scene)
    {
    case 0:
-      g_sndSystem->playSE((SndID)0x1cf5,0x0,0x0,0x0,0xffffffff);
+      g_sndSystem->playSE((SndID)0x1cf5, 0x0, 0x0, 0x0, 0xffffffff);
       break;
    case 3:
-      g_sndSystem->playSE((SndID)0x1cf6,0x0,0x0,0x0,0xffffffff);
+      g_sndSystem->playSE((SndID)0x1cf6, 0x0, 0x0, 0x0, 0xffffffff);
       break;
    case 1:
    case 2:
    case 4:
-      g_sndSystem->playSE((SndID)0x1cf7,0x0,0x0,0x0,0xffffffff);
+      g_sndSystem->playSE((SndID)0x1cf7, 0x0, 0x0, 0x0, 0xffffffff);
       break;
    }
 }
+void stVillage::createObjBg(int index)
+{
+   int sceneBit = 0;
+   grVillage *bg = NULL;
+   switch (index)
+   {
+   case 0:
+      bg = grVillage::create(0, "yStgVillageChikeAll", "grVillageMainBg");
+      break;
+   case 1:
+      bg = grVillage::create(1, "StgVillageMadoHikari", "grVillageMainBgLight");
+      sceneBit = 0xC;
+      break;
+   case 2:
+      bg = grVillage::create(2, "StgVillageMainStage", "grVillageMainStage");
+   case 3:
+      bg = grVillage::create(3, "zStgVillageHikari", "grVillageMainStageLight");
+      sceneBit = 0x1C;
+      break;
+   case 4:
+      grVillage::create(0xe, "aStgVillageSky00Star", "grVillageStar");
+      sceneBit = 0x18;
+      break;
+   case 5:
+      bg = grVillage::create(0x11, "aStgVillageSky01", "grVillageSky");
+      break;
+   }
 
-void Ground::setStageData(float* stageData) {
+   if (bg != NULL)
+   {
+      this->addGround(bg);
+      bg->startup(fileData, 0, 0);
+      bg->setStageData(stageData);
+      bg->setSceneWork((u32 *)&this->scene);
+      bg->setSceneBit(sceneBit);
+      bg->setStateWork((u32 *)&this->state);
+      bg->setPosGuestWork((u32 *)&this->posGuest);
+   }
+}
+void stVillage::createObjSky(int index)
+{
+   if (index == 5)
+   {
+      grVillageSky *sky = grVillageSky::create(0x11, "aStgVillageSky01", "grVillageSky");
+      if (sky == NULL)
+         return;
+
+      this->addGround(sky);
+      sky->startup(fileData, 0, 0);
+      sky->setStageData(stageData);
+      sky->setSceneWork((u32 *)&this->scene);
+      sky->setStateWork((u32 *)&this->state);
+   }
+}
+void stVillage::createObjBalloon(int index)
+{
+   if (index == 0xE)
+   {
+      // grVillageBalloon* balloon = grVillageBalloon::create(0xb,"StgVillageBalloon","grVillageBalloon");
+   }
+}
+void Ground::setStageData(float *stageData)
+{
    this->stageData = stageData;
 }
-void stVillage::startFighterEvent() {
+void stVillage::startFighterEvent()
+{
    return;
 }
-int stVillage::initializeFighterAttackRatio() {
+int stVillage::initializeFighterAttackRatio()
+{
    return 0;
 }
-int stVillage::helperStarWarp(){
+int stVillage::helperStarWarp()
+{
    return 0;
 }
-bool stVillage::isSimpleBossBattleMode() {
+bool stVillage::isSimpleBossBattleMode()
+{
    return false;
 }
-bool stVillage::isBossBattleMode() {
+bool stVillage::isBossBattleMode()
+{
    return false;
 }
-bool stVillage::isCameraLocked() {
+bool stVillage::isCameraLocked()
+{
    return true;
 }
-void stVillage::notifyTimmingGameStart(){
+void stVillage::notifyTimmingGameStart()
+{
    return;
 }
-float stVillage::getFrameRuleTime() {
+float stVillage::getFrameRuleTime()
+{
    return this->frameRuleTime;
 }
-void stVillage::setFrameRuleTime(float newTime) {
+void stVillage::setFrameRuleTime(float newTime)
+{
    this->frameRuleTime = newTime;
 }
-bool stVillage::isNextStepBgmEqualNowStepBgmFromFlag() {
+bool stVillage::isNextStepBgmEqualNowStepBgmFromFlag()
+{
    return false;
 }
-float stVillage::getBgmPlayOffsetFrame(){
+float stVillage::getBgmPlayOffsetFrame()
+{
    return BGM_PLAY_OFFSET_FRAME;
 }
-float stVillage::getBgmVolume() {
+float stVillage::getBgmVolume()
+{
    return BGM_VOLUME;
 }
-void stVillage::setBgmChange(float unk, u32 unk1, u32 unk2) {
+void stVillage::setBgmChange(float unk, u32 unk1, u32 unk2)
+{
    this->unk2 = unk1;
    this->unk3 = unk2;
    this->unk4 = unk;
 }
-void stVillage::getBgmChangeID(u32 unk1, float unk2) {
+void stVillage::getBgmChangeID(u32 unk1, float unk2)
+{
    unk1 = this->unk3;
    unk2 = this->unk4;
 }
-bool stVillage::isBgmChange() {
+bool stVillage::isBgmChange()
+{
    return this->unk2;
 }
-int stVillage::getBgmOptionID() {
+int stVillage::getBgmOptionID()
+{
    return 0;
 }
-int stVillage::getNowStepBgmID() {
+int stVillage::getNowStepBgmID()
+{
    return 0;
 }
-int stVillage::getBgmID() {
+int stVillage::getBgmID()
+{
    return 0;
 }
-int stVillage::getBgmIDOverload() {
+int stVillage::getBgmIDOverload()
+{
    return 0;
 }
-void stVillage::appearanceFighterLocal() {
+void stVillage::appearanceFighterLocal()
+{
    return;
 }
 // TODO
 // stubbed because i can't figure out where this is called
-int stVillage::getScrollDir(u32 unk1) {
+int stVillage::getScrollDir(u32 unk1)
+{
    return 0;
 }
-int stVillage::getDefaultLightSetIndex(){ 
+int stVillage::getDefaultLightSetIndex()
+{
    return 0x14;
 }
-int stVillage::getAIRange() {
-   return (u32) &this->aiRange;
+int stVillage::getAIRange()
+{
+   return (u32) & this->aiRange;
 }
-bool stVillage::isAdventureStage(){
+bool stVillage::isAdventureStage()
+{
    return false;
 }
-int stVillage::getPokeTrainerDrawLayer() {
+int stVillage::getPokeTrainerDrawLayer()
+{
    return 0;
 }
-float stVillage::getPokeTrainerPositionZ() {
+float stVillage::getPokeTrainerPositionZ()
+{
    return POKETRAINER_Z;
 }
-void stVillage::getPokeTrainerPointData(int * unk, int unk2){
+void stVillage::getPokeTrainerPointData(int *unk, int unk2)
+{
    return;
 }
-int stVillage::getPokeTrainerPointNum() {
+int stVillage::getPokeTrainerPointNum()
+{
    return 0;
 }
-bool stVillage::isReStartSamePoint() {
+bool stVillage::isReStartSamePoint()
+{
    return true;
 }
-int stVillage::getWind2ndOnlyData() {
-   return (u32) &this->wndOnlyData2;
+int stVillage::getWind2ndOnlyData()
+{
+   return (u32) & this->wndOnlyData2;
 }
-bool stVillage::isBamperVector() {
+bool stVillage::isBamperVector()
+{
    return true;
 }
-int stVillage::getFinalTechniqColor() {
+int stVillage::getFinalTechniqColor()
+{
    return 0x14000496;
 }
 
-void stVillage::createObjBalloon(int index){
-   if(index == 0xE){
-      //grVillageBalloon* balloon = grVillageBalloon::create(0xb,"StgVillageBalloon","grVillageBalloon");
-   }
+template <int I, typename T>
+T *stClassInfoImpl<I, T>::create()
+{
+   T *stage = new (StageInstance) T(I);
+   return stage;
 }
 
-template<int I, typename T>
-T* stClassInfoImpl<I, T>::create(){
-    T* stage = new(StageInstance) T(I);
-    return stage;
+template <int I, typename T>
+stClassInfoImpl<I, T>::~stClassInfoImpl()
+{
+   setClassInfo(I, 0);
 }
 
-template<int I, typename T>
-stClassInfoImpl<I, T>::~stClassInfoImpl() {
-   setClassInfo(I,0);
-}
-
-template<int I, typename T>
-void stClassInfoImpl<I, T>::preload() {
+template <int I, typename T>
+void stClassInfoImpl<I, T>::preload()
+{
    return;
 }
