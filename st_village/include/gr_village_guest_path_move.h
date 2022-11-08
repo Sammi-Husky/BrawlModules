6601edca15d4bee85224a860f266f8dd6e963043
Sammi Husky <sammi-husky@live.com>
2022-11-08 15:38:51 -0800

    Update grVillageGuestPathMove

--- st_village/include/gr_village_guest_path_move.h
+++ st_village/include/gr_village_guest_path_move.h
@@ -2,38 +2,41 @@
 
 #include "gr_village.h"
 #include <StaticAssert.h>
-#include <st/st_se_util.h>
-#include <types.h>
-
-// const float UNK_FLOAT_1 = 0;
+#include <snd/snd_id.h>
+#include <st/stage.h>
 
 class grVillageGuestPathMove : public grVillage {
-private:
-    // char field_0x00;               // 0x00
-    // char field_0x01;               // 0x01
-    // char field_0x02;               // 0x02
-    // char _0;                       // 0x03
-    // float field_0x04;              // 0x04
-    // float field_0x08;              // 0x08
-    // StSeUtil::SeSeq m_seq;         // 0x0C
-    // char _1;                       // 0x2B
-    // char field_0x2C[0x0C];         // 0x2C
-    // char field_0x38[0x0C];         // 0x38
-    // char _2[0x18];                 // 0x44
-    // snd3DGenerator m_sndGenerator; // 0x5C
+protected:
+    char m_type;                   // 0x00
+    char field_0x01;               // 0x01
+    char field_0x02;               // 0x02
+    char _0;                       // 0x03
+    float field_0x04;              // 0x04
+    float field_0x08;              // 0x08
+    StSeUtil::SeSeq m_seq;         // 0x0C
+    char field_0x2C[0x0C];         // 0x2C
+    char field_0x38[0x0C];         // 0x38
+    char _2[0x4];                  // 0x44
+    SndID m_sndID;                 // 0x48
+    SndID m_seqID;                 // 0x4C
+    float field_0x50;              // 0x50
+    float field_0x54;              // 0x54
+    float field_0x58;              // 0x58
+    snd3DGenerator m_sndGenerator; // 0x5C
 
 public:
-    static grVillageGuestPathMove* create(int mdlIndex, const char* tgtNodeName, const char* taskName);
+    static grVillageGuestPathMove* create(int mdlIndex, char* tgtNodeName, char* taskName);
 
     inline grVillageGuestPathMove(char* taskName);
+    virtual void updateYakumono(float deltaFrame);
+    virtual void updateMove(float deltaFrame);
+    virtual void updateCallback(float deltaFrame);
+    virtual void setMotion(u32 unk1, u32 unk2, u32 unk3, float* unkFloat);
+    virtual void setType(u32 unk);
+    virtual ~grVillageGuestPathMove();
+
     void update(float frameDelta);
     void setMotionRatio(float speedMult);
-    void updateYakumono();
-    void updateMove(float unk);
-    void updateCallback();
-    void setType(u32 unk);
-    ~grVillageGuestPathMove();
 
-    STATIC_CHECK(sizeof(grVillageGuestPathMove) == 0x18C)
-    PRINT_SIZEOF(grVillageGuestPathMove)
+    STATIC_CHECK(sizeof(grVillageGuestPathMove) == 0x1CC)
 };
