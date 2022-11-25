0deba70e02160493f1b5c0067b60ddcd7c1f935a
Kapedani <kapedanipm@gmail.com>
2022-11-25 01:48:52 -0500

    Included disassembled rel for sora_enemy_vs

---
+++ st_demo/reltools/sora_enemy_vs/Enemies/rodata/emBosspackun.asm
@@ -0,0 +1,14 @@
+loc_1118:
+        .4byte 0x42C80000
+        .4byte 0x3F800000
+        .4byte 0x00000000
+        .4byte 0x00000000
+        
+loc_FC0:
+        .4byte 0x3F800000
+loc_FC4:
+        .4byte 0xBF800000
+        .4byte 0x00000000
+        .4byte 0x42C80000
+        .4byte 0x43300000
+        .4byte 0x00000000
