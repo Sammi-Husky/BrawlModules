5e54ab27295299e802d515935c6a873e6e4b8510
Kapedani <kapedanipm@gmail.com>
2022-12-18 01:36:16 -0500

    [targetsmash] Rotation from node

--- st_targetsmash/include/gr_targetsmash_spring.h
+++ st_targetsmash/include/gr_targetsmash_spring.h
@@ -4,14 +4,18 @@
 #include <gr/gr_gimmick_spring.h>
 
 class grTargetSmashSpring : public grGimmickSpring {
-
+    float m_rot;
+    float m_bounce;
 public:
     grTargetSmashSpring(char* taskName) : grGimmickSpring(taskName) {
-
+        m_rot = 0.0;
+        m_bounce = 5.0;
     }
-    virtual ~grTargetSmashSpring() {
-    }
+    virtual ~grTargetSmashSpring() { }
+    virtual void startup(gfArchive* archive, u32 unk1, u32 unk2);
+    virtual void update(float deltaFrame);
     virtual void setMotionOff();
+    virtual void presentShootEvent();
 
     static grTargetSmashSpring* create(int mdlIndex, char* taskName);
 };
