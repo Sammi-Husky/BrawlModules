3fe3a795c76f30521e186b164179f8d586cb5e8c
Max Parisi <max.parisi2020@gmail.com>
2023-02-21 20:34:51 -0500

    Add st_otrain and MWCC4_1
    MWCC4_1 (v4.1 build 60831) gives a more accurate
    match for st_onlinetrainning.cpp

---
+++ st_otrain/source/mo_stage.cpp
@@ -0,0 +1,36 @@
+#include <st/module.h>
+
+extern "C" {
+typedef void (*PFN_voidfunc)();
+__declspec(section ".init") extern PFN_voidfunc _ctors[];
+__declspec(section ".init") extern PFN_voidfunc _dtors[];
+
+void _prolog();
+void _epilog();
+void _unresolved();
+}
+
+void _prolog()
+{
+    // Run global constructors
+    PFN_voidfunc* ctor;
+    for (ctor = _ctors; *ctor; ctor++)
+    {
+        (*ctor)();
+    }
+}
+
+void _epilog()
+{
+    // run the global destructors
+    PFN_voidfunc* dtor;
+    for (dtor = _dtors; *dtor; dtor++)
+    {
+        (*dtor)();
+    }
+}
+
+void _unresolved()
+{
+    moUnResolvedMessage("mo_stage.cpp");
+}
