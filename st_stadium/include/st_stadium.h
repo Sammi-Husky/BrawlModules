04fbf72bc77ca3da304880b11b6ff4aa3a9e080b
Kapedani <kapedanipm@gmail.com>
2023-05-02 18:39:38 -0400

    [stadium] Initial commit

---
+++ st_stadium/include/st_stadium.h
@@ -0,0 +1,55 @@
+#pragma once
+
+#include <StaticAssert.h>
+#include <st/st_melee.h>
+#include <types.h>
+
+const float BGM_PLAY_OFFSET_FRAME = 0.0f;
+const float BGM_VOLUME = 1.0f;
+const float SCROLL_DIR = 0.0f;
+const float POKETRAINER_Z = 0.0f;
+
+class stStadium : public stMelee {
+public:
+    stStadium() : stMelee("stStadium", Stages::Final){};
+    static stStadium* create();
+    grGimmickWindData2nd* getWind2ndOnlyData();
+    bool isReStartSamePoint();
+    int getPokeTrainerPointNum();
+    void startFighterEvent();
+    int initializeFighterAttackRatio();
+    int helperStarWarp();
+    bool isSimpleBossBattleMode();
+    bool isBossBattleMode();
+    bool isCameraLocked();
+    void notifyTimmingGameStart();
+    float getFrameRuleTime();
+    void setFrameRuleTime(float newTime);
+    bool isNextStepBgmEqualNowStepBgmFromFlag();
+    float getBgmPlayOffsetFrame();
+    float getBgmVolume();
+    void setBgmChange(float unk, u32 unk1, u32 unk2);
+    void getBgmChangeID(u32 unk1, float unk2);
+    bool isBgmChange();
+    int getBgmOptionID();
+    int getNowStepBgmID();
+    int getBgmID();
+    int getBgmIDOverload();
+    void appearanceFighterLocal();
+    int getScrollDir(u32 unk1);
+    int getDefaultLightSetIndex();
+    stRange* getAIRange();
+    bool isAdventureStage();
+    int getPokeTrainerDrawLayer();
+    float getPokeTrainerPositionZ();
+    void getPokeTrainerPointData(int* unk, int unk2);
+
+    virtual void createObj();
+    virtual bool loading();
+    virtual void update(float deltaFrame);
+    virtual int getFinalTechniqColor();
+    virtual bool isBamperVector();
+    virtual ~stStadium() { this->releaseArchive(); };
+
+    STATIC_CHECK(sizeof(stStadium) == 0x1D8)
+};
---
+++ st_stadium/source/st_stadium.cpp
@@ -0,0 +1,201 @@
+#include "st_stadium.h"
+#include "gr_final.h"
+#include <memory.h>
+#include <st/st_class_info.h>
+
+static stClassInfoImpl<Stages::Final, stStadium> classInfo = stClassInfoImpl<Stages::Final, stStadium>();
+
+stStadium* stStadium::create()
+{
+    stStadium* stage = new (Heaps::StageInstance) stStadium();
+    return stage;
+}
+bool stStadium::loading()
+{
+    return true;
+}
+void stStadium::update(float deltaFrame)
+{
+    return;
+}
+
+void stStadium::createObj()
+{
+    testStageParamInit(m_fileData, 0xA);
+    testStageDataInit(m_fileData, 0x14, 1);
+    grFinal* ground = grFinal::create(1, "", "grFinalMainBg");
+    if (ground != NULL)
+    {
+        addGround(ground);
+        ground->startup(m_fileData, 0, 0);
+        ground->setStageData(m_stageData);
+        ground->setDontMoveGround();
+    }
+    ground = grFinal::create(2, "", "grFinalStage");
+    if (ground != NULL)
+    {
+        addGround(ground);
+        ground->startup(m_fileData, 0, 0);
+        ground->setStageData(m_stageData);
+        ground->setDontMoveGround();
+    }
+    createCollision(m_fileData, 2, NULL);
+    initCameraParam();
+    void* posData = m_fileData->getData(Data_Type_Model, 0x64, 0xfffe);
+    if (posData == NULL)
+    {
+        // if no stgPos model in pac, use defaults
+        createStagePositions();
+    }
+    else
+    {
+        // stgPosWrapper stgPos = {posData}; // creates wrapper on the stack
+        createStagePositions(&posData);
+    }
+    createWind2ndOnly();
+    loadStageAttrParam(m_fileData, 0x1E);
+    nw4r::g3d::ResFileData* scnData = static_cast<nw4r::g3d::ResFileData*>(m_fileData->getData(Data_Type_Scene, 0, 0xfffe));
+    registScnAnim(scnData, 0);
+    initPosPokeTrainer(1, 0);
+    createObjPokeTrainer(m_fileData, 0x65, "PokeTrainer00", this->m_unk, 0x0);
+}
+
+void Ground::setStageData(void* stageData)
+{
+    this->m_stageData = stageData;
+}
+void stStadium::startFighterEvent()
+{
+    return;
+}
+int stStadium::initializeFighterAttackRatio()
+{
+    return 0;
+}
+int stStadium::helperStarWarp()
+{
+    return 0;
+}
+bool stStadium::isSimpleBossBattleMode()
+{
+    return false;
+}
+bool stStadium::isBossBattleMode()
+{
+    return false;
+}
+bool stStadium::isCameraLocked()
+{
+    return true;
+}
+void stStadium::notifyTimmingGameStart()
+{
+    return;
+}
+float stStadium::getFrameRuleTime()
+{
+    return this->m_frameRuleTime;
+}
+void stStadium::setFrameRuleTime(float newTime)
+{
+    this->m_frameRuleTime = newTime;
+}
+bool stStadium::isNextStepBgmEqualNowStepBgmFromFlag()
+{
+    return false;
+}
+float stStadium::getBgmPlayOffsetFrame()
+{
+    return BGM_PLAY_OFFSET_FRAME;
+}
+float stStadium::getBgmVolume()
+{
+    return BGM_VOLUME;
+}
+void stStadium::setBgmChange(float unk, u32 unk1, u32 unk2)
+{
+    this->m_unk2 = unk1;
+    this->m_unk3 = unk2;
+    this->m_unk4 = unk;
+}
+void stStadium::getBgmChangeID(u32 unk1, float unk2)
+{
+    unk1 = this->m_unk3;
+    unk2 = this->m_unk4;
+}
+bool stStadium::isBgmChange()
+{
+    return this->m_unk2;
+}
+int stStadium::getBgmOptionID()
+{
+    return 0;
+}
+int stStadium::getNowStepBgmID()
+{
+    return 0;
+}
+int stStadium::getBgmID()
+{
+    return 0;
+}
+int stStadium::getBgmIDOverload()
+{
+    return 0;
+}
+void stStadium::appearanceFighterLocal()
+{
+    return;
+}
+// TODO
+// stubbed because i can't figure out where this is called
+int stStadium::getScrollDir(u32 unk1)
+{
+    return 0;
+}
+int stStadium::getDefaultLightSetIndex()
+{
+    return 0x14;
+}
+stRange* stStadium::getAIRange()
+{
+    return &this->m_aiRange;
+}
+bool stStadium::isAdventureStage()
+{
+    return false;
+}
+int stStadium::getPokeTrainerDrawLayer()
+{
+    return 0;
+}
+float stStadium::getPokeTrainerPositionZ()
+{
+    return POKETRAINER_Z;
+}
+void stStadium::getPokeTrainerPointData(int* unk, int unk2)
+{
+    return;
+}
+int stStadium::getPokeTrainerPointNum()
+{
+    return 0;
+}
+bool stStadium::isReStartSamePoint()
+{
+    return true;
+}
+grGimmickWindData2nd* stStadium::getWind2ndOnlyData()
+{
+    return m_windAreaData2nd;
+}
+bool stStadium::isBamperVector()
+{
+    return true;
+}
+int stStadium::getFinalTechniqColor()
+{
+    return 0x14000496;
+}
+
+ST_CLASS_INFO;
