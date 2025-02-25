cb260f1a796a8d89a95add5199cfa564d60160e6
Kapedani <kapedanipm@gmail.com>
2025-02-25 02:53:48 -0500

    [stTargetSmash] Shade refactoring and polishing

--- st_targetsmash/include/st_targetsmash_shade.h
+++ st_targetsmash/include/st_targetsmash_shade.h
@@ -7,11 +7,12 @@
 #include <st/loader/st_loader_manager.h>
 #include <if/if_mngr.h>
 #include <ft/ft_manager.h>
+#include <sc/sc_melee.h>
+#include <stdio.h>
 #include <OS/OSError.h>
 
-template <u32 L>
-class stTargetSmashShade {
-
+class stTargetSmashShadeInterface {
+public:
     enum State {
         State_Inactive = 0x0,
         State_Start = 0x1,
@@ -30,13 +31,12 @@ class stTargetSmashShade {
     s8 followPlayerId;
     State state : 8;
     int currentFrame;
-    FrameInfo m_frameInfo[L];
 
-public:
-    stTargetSmashShade(gmCharacterKind charKind, u8 costumeId,  u8 colorFileIdx, u8 followPlayerId, u8 playerId) {
-        this->initialize(charKind, costumeId, colorFileIdx, followPlayerId, playerId);
-    };
-    void initialize(gmCharacterKind charKind, u8 costumeId, u8 colorFileIdx, u8 followPlayerId, u8 playerId) {
+    virtual void setFrameInfo(int frame, FrameInfo* frameInfo);
+    virtual FrameInfo getFrameInfo(int frame);
+    virtual u32 getFollowLength();
+
+    virtual void initialize(gmCharacterKind charKind, u8 costumeId, u8 colorFileIdx, u8 followPlayerId, u8 playerId) {
         this->currentFrame = 0;
         this->followPlayerId = followPlayerId;
         this->playerId = playerId;
@@ -48,8 +48,8 @@ public:
         g_GameGlobal->m_modeMelee->m_playersInitData[playerId].m_stockCount = 1;
         g_GameGlobal->m_modeMelee->m_playersInitData[playerId].m_teamNo = 5;
         g_GameGlobal->m_modeMelee->m_playersInitData[playerId].m_isNoVoice = true;
-    };
-    void update(float deltaFrame) {
+    }
+    virtual void update(float deltaFrame) {
         //g_stLoaderManager->m_loaderPlayers[this->playerId]->removeInfo();
         if (g_IfMngr != NULL) {
             g_IfMngr->m_ifPlayers[this->playerId]->disappear();
@@ -61,38 +61,43 @@ public:
             Fighter *fighter = g_ftManager->getFighter(entryId, -1);
             Fighter *followFighter = g_ftManager->getFighter(followEntryId, -1);
 
-            switch(this->state) {
+            switch (this->state) {
                 case State_Inactive: {
-                    fighter->m_moduleAccesser->getStatusModule()->changeStatus(Fighter::Status_Test_Motion, fighter->m_moduleAccesser);
+                    fighter->m_moduleAccesser->getStatusModule()->changeStatus(Fighter::Status_Test_Motion,
+                                                                               fighter->m_moduleAccesser);
                     fighter->m_moduleAccesser->getCollisionHitModule()->setXluGlobal(0);
                     fighter->m_moduleAccesser->getEffectModule()->removeCommon(0);
-                    fighter->m_moduleAccesser->getColorBlendModule()->setSubColor((GXColor){0, 0, 0, 0xff}, true);
+                    fighter->m_moduleAccesser->getColorBlendModule()->setSubColor((GXColor) {0, 0, 0, 0xff}, true);
                     fighter->m_moduleAccesser->getCameraModule()->setEnableCamera(0, -1);
                     fighter->m_moduleAccesser->getVisibilityModule()->setWhole(0);
                     fighter->m_moduleAccesser->getAreaModule()->enableArea(-1, false, 0);
                     Vec3f pos = followFighter->m_moduleAccesser->getPostureModule()->getPos();
                     fighter->m_moduleAccesser->getPostureModule()->setPos(&pos);
 
-                    //fighter->m_moduleAccesser->getCollisionAttackModule()->sleep(true); // Only for ghosts
-
                     this->state = State_Start;
 
                 }
                     break;
 
-                case State_Follow:
-                {
-                    fighter->m_moduleAccesser->getPostureModule()->setPos(&this->m_frameInfo[this->currentFrame].pos);
-                    fighter->m_moduleAccesser->getPostureModule()->setRot(&this->m_frameInfo[this->currentFrame].rot, 0);
-                    fighter->m_moduleAccesser->getPostureModule()->setLr(this->m_frameInfo[this->currentFrame].lr);
+                case State_Follow: {
+                    FrameInfo frameInfo = this->getFrameInfo(this->currentFrame);
+                    fighter->m_moduleAccesser->getPostureModule()->setPos(&frameInfo.pos);
+                    fighter->m_moduleAccesser->getPostureModule()->setRot(&frameInfo.rot, 0);
+                    fighter->m_moduleAccesser->getPostureModule()->setLr(frameInfo.lr);
                     fighter->m_moduleAccesser->getPostureModule()->updateRotYLr();
-                    if (fighter->m_moduleAccesser->getMotionModule()->getKind() != this->m_frameInfo[this->currentFrame].motionKind) {
-                        soMotionChangeParam changeParam = {this->m_frameInfo[this->currentFrame].motionKind, this->m_frameInfo[this->currentFrame].motionFrame, 0.0, 0, 0, true, 0};
+                    const char* motionName = fighter->m_moduleAccesser->getMotionModule()->getName(frameInfo.motionKind, true);
+                    if (motionName == NULL || strcmp(motionName, "NONE") == 0 || strcmp(motionName, "") == 0) {
+                        frameInfo.motionKind = Fighter::Motion_Catapult;
+                        frameInfo.motionFrame = 1.0;
+                    }
+
+                    if (fighter->m_moduleAccesser->getMotionModule()->getKind() != frameInfo.motionKind) {
+                        soMotionChangeParam changeParam = {frameInfo.motionKind, frameInfo.motionFrame, 0.0, 0, 0,
+                                        true, 0};
                         fighter->m_moduleAccesser->getMotionModule()->changeMotion(&changeParam);
                         fighter->m_moduleAccesser->getAnimCmdModule()->deactivate(fighter->m_moduleAccesser);
-                    }
-                    else {
-                        fighter->m_moduleAccesser->getMotionModule()->setFrame(this->m_frameInfo[this->currentFrame].motionFrame);
+                    } else {
+                        fighter->m_moduleAccesser->getMotionModule()->setFrame(frameInfo.motionFrame);
                     }
                     break;
                 }
@@ -102,18 +107,29 @@ public:
             int statusKind = fighter->m_moduleAccesser->getStatusModule()->getStatusKind();
             fighter->m_moduleAccesser->getGroundModule()->setCorrect(soGroundShapeImpl::Correct_None, 0);
 
-            this->m_frameInfo[this->currentFrame].pos = followFighter->m_moduleAccesser->getPostureModule()->getPos();
-            this->m_frameInfo[this->currentFrame].rot = followFighter->m_moduleAccesser->getPostureModule()->getRot(0);
-            this->m_frameInfo[this->currentFrame].lr = followFighter->m_moduleAccesser->getPostureModule()->getLr();
-            if (followFighter->m_moduleAccesser->getVisibilityModule()->getWhole() == true) {
-                this->m_frameInfo[this->currentFrame].motionKind = followFighter->m_moduleAccesser->getMotionModule()->getKind();
-                this->m_frameInfo[this->currentFrame].motionFrame = followFighter->m_moduleAccesser->getMotionModule()->getFrame();
-            }
-            else {
-                this->m_frameInfo[this->currentFrame].motionKind = Fighter::Motion_Catapult;
-                this->m_frameInfo[this->currentFrame].motionFrame = 1.0;
+            FrameInfo frameInfo;
+            frameInfo.pos = followFighter->m_moduleAccesser->getPostureModule()->getPos();
+            frameInfo.rot = followFighter->m_moduleAccesser->getPostureModule()->getRot(0);
+            frameInfo.lr = followFighter->m_moduleAccesser->getPostureModule()->getLr();
+//            if (followFighter->m_moduleAccesser->getStatusModule()->getStatusKind() == Fighter::Status_Dead) {
+//                frameInfo.motionKind = (frameInfo.lr >= 0.0) ? Fighter::Motion_Appeal_Lw_R : Fighter::Motion_Appeal_Lw_L;
+//                if (followFighter->m_moduleAccesser->getStatusModule()->getPrevStatusKind(0) != Fighter::Status_Dead) {
+//                    frameInfo.motionFrame = 0;
+//                }
+//                else {
+//                    frameInfo.motionFrame += deltaFrame;
+//                }
+            } else if (followFighter->m_moduleAccesser->getVisibilityModule()->getWhole() == true) {
+                frameInfo.motionKind = followFighter->m_moduleAccesser->getMotionModule()->getKind();
+                frameInfo.motionFrame = followFighter->m_moduleAccesser->getMotionModule()->getFrame();
+            } else {
+                frameInfo.motionKind = Fighter::Motion_Catapult;
+                frameInfo.motionFrame = 1.0;
             }
 
+
+
+            this->setFrameInfo(this->currentFrame, &frameInfo);
 
             if (this->state != State_Inactive) {
                 this->currentFrame++;
@@ -140,7 +156,7 @@ public:
                     attackData.m_isDeath100 = true;
                     fighter->m_moduleAccesser->getCollisionAttackModule()->set(0, 0, &attackData);
 
-                    // TODO: Make sure ignore invincible moves can't detect hurtbox?
+                    // TODO: Make sure ignore invincible moves can't detect hurtbox and things can't detect shade?
                     // TODO: Keep track of visibility, change anim to something other than tpose
                 }
             }
@@ -150,10 +166,28 @@ public:
 
         }
     }
-    u32 getFollowLength() { return L; }
+
+};
+
+template <u32 L>
+class stTargetSmashShade : public stTargetSmashShadeInterface {
+    FrameInfo m_frameInfos[L];
+
+public:
+    stTargetSmashShade(gmCharacterKind charKind, u8 costumeId,  u8 colorFileIdx, u8 followPlayerId, u8 playerId) {
+        this->initialize(charKind, costumeId, colorFileIdx, followPlayerId, playerId);
+    };
+    virtual void setFrameInfo(int frame, FrameInfo* frameInfo) {
+        this->m_frameInfos[frame] = *frameInfo;
+    }
+    virtual FrameInfo getFrameInfo(int frame) {
+        if (frame < 0) {
+            return this->m_frameInfos[0];
+        }
+        return this->m_frameInfos[frame];
+    }
+    virtual u32 getFollowLength() { return L; }
 
 };
 
 // TODO: Handle transforming characters like Sheik/Zelda? and Ice Climbers
-// TODO: Ensure things can't target shade?
-// TODO: Disable screen shakes (and effects?)
