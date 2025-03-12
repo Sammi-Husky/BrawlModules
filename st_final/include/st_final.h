df7bbe6e01d5b7cb406e3e67b4b40a3664865845
Kapedani <kapedanipm@gmail.com>
2025-03-12 11:24:41 -0400

    Removed functions already in stMelee

--- st_final/include/st_final.h
+++ st_final/include/st_final.h
@@ -16,36 +16,6 @@ public:
 
     };
     static stFinal* create();
-    grGimmickWindData2nd* getWind2ndOnlyData();
-    bool isReStartSamePoint();
-    int getPokeTrainerPointNum();
-    void startFighterEvent();
-    int initializeFighterAttackRatio();
-    int helperStarWarp();
-    bool isSimpleBossBattleMode();
-    bool isBossBattleMode();
-    bool isCameraLocked();
-    void notifyTimmingGameStart();
-    float getFrameRuleTime();
-    void setFrameRuleTime(float newTime);
-    bool isNextStepBgmEqualNowStepBgmFromFlag();
-    float getBgmPlayOffsetFrame();
-    float getBgmVolume();
-    void setBgmChange(float unk, u32 unk1, u32 unk2);
-    void getBgmChangeID(u32 unk1, float unk2);
-    bool isBgmChange();
-    int getBgmOptionID();
-    int getNowStepBgmID();
-    int getBgmID();
-    int getBgmIDOverload();
-    void appearanceFighterLocal();
-    int getScrollDir(u32 unk1);
-    int getDefaultLightSetIndex();
-    Rect2D* getAIRange();
-    bool isAdventureStage();
-    int getPokeTrainerDrawLayer();
-    float getPokeTrainerPositionZ();
-    void getPokeTrainerPointData(int* unk, int unk2);
 
     virtual void createObj();
     virtual bool loading();
